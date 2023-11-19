7be2c53e7adc9e37deaf31fbcae0ce75bf5b5a79
Uta <usupovaua2707@mail.ru>
2023-11-19 15:11:06 +0300

    Angular-new-error

--- burgers_intensiv/burgers/src/app/app.component.ts
+++ burgers_intensiv/burgers/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
+  title = 'Бургеры';
+  currency = '$';
+
   scrollTo(target: HTMLElement) {
-  target.scrollIntoView({behavior: "smooth"});
+    target.scrollIntoView({ behavior: "smooth" });
   }
 }
-
 
